24f03bebd925e1bac27b77bb999f0c67e5893a03
Daniel Vadil <dvadz@yahoo.com>
2022-03-04 14:43:41 -0500

    Made a user model for username and password, required it and connected to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const User = require("./models/user");
 const ejs = require("ejs");
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/myAuthFromScratch");
 
 app.get("/", (req, res) => {
   res.send("HOME");
---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: "Username is required",
+  },
+  passordHash: {
+    type: String,
+    required: "Password is required",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
